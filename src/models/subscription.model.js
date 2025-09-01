bbcb936e55fb0bfe892de65fb8813092b9c7dd01
Prem <panhaleprem@gmail.com>
2025-09-01 19:51:11 +0530

    Aggregation Pipelines

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,6 @@
 import mongoose,{Schema} from "mongoose";
 
-const subsciptionSchema = new Schema({
+const subscriptionSchema = new Schema({
     subscriber: {
         type: Schema.Types.ObjectId,
         ref: "User"
@@ -13,4 +13,4 @@ const subsciptionSchema = new Schema({
 
 }, {timestamps: true})
 
-export const Subsciption = mongoose.model("Subsciption", subsciptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
